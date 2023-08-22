a4a3e29d94828156ba714862434039d4a780e5c8
Ryan Bradley <ryanbradley315@gmail.com>
2023-08-22 16:44:11 -0500

    Add routes to App component.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,23 @@
 import { Routes, Route } from 'react-router-dom'
 import './App.css'
 import Suggestions from './routes/suggestions/Suggestions'
+import Roadmap from './routes/roadmap/Roadmap'
+import FeedbackForm from './components/feedbackForm/FeedbackForm'
+import Feedback from './routes/feedback/Feedback'
 
 function App() {
   return (
     <Routes>
       
       <Route path='/' element={<Suggestions />} />
+
+      <Route path='/roadmap' element={<Roadmap />} />
+
+      <Route path='/new' element={<FeedbackForm />} />
+
+      <Route path='/edit' element={<FeedbackForm />} />
+
+      <Route path='/:id' element={<Feedback />} />
 
     </Routes>
   )
--- client/src/routes/feedback/Feedback.tsx
+++ client/src/routes/feedback/Feedback.tsx
@@ -1,12 +1,15 @@
+import { useParams } from 'react-router-dom'
 import styles from './feedback.module.css'
 
 export default function Feedback() {
+    const { id } = useParams()
+
     return (
         <main
             className={styles.main}
         >
 
-            Feedback
+            { id }
 
         </main>
     )
