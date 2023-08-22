ffe4a35610fb887fd186622bd1cce1a385025eb2
Ryan Bradley <ryanbradley315@gmail.com>
2023-08-22 16:28:59 -0500

    Add basic architecture for site layout.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,14 @@
+import { Routes, Route } from 'react-router-dom'
 import './App.css'
+import Suggestions from './routes/suggestions/Suggestions'
 
 function App() {
   return (
-    <>
-      Hello
-    </>
+    <Routes>
+      
+      <Route path='/' element={<Suggestions />} />
+
+    </Routes>
   )
 }
 
---
+++ client/src/routes/suggestions/Suggestions.tsx
@@ -0,0 +1,25 @@
+import styles from './suggestions.module.css'
+
+export default function Suggestions() {
+    return (
+        <>
+        
+            <header>
+                Header
+            </header>
+
+            <main
+                className={styles.main}
+            >
+
+                Main
+
+            </main>
+
+            <footer>
+                Footer
+            </footer>
+        
+        </>
+    )
+}
