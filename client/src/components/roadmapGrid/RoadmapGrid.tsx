96f5760ccd108e74c54c66fcc1c2c49766c351f8
Ryan Bradley <ryanbradley315@gmail.com>
2023-08-24 08:30:55 -0500

    Update roadmap grid logic to generate filtered status categories and display feedback count based on these filtered categories.

--- client/src/components/roadmapGrid/RoadmapGrid.tsx
+++ client/src/components/roadmapGrid/RoadmapGrid.tsx
@@ -2,12 +2,24 @@ import { useState } from 'react'
 import { SAMPLE_FEEDBACK } from '../../sampleData/feedback'
 import FeedbackCard from '../feedbackCard/FeedbackCard'
 import styles from './roadmapGrid.module.css'
+import { useQuery } from '@tanstack/react-query'
 
 export default function RoadmapGrid() {
     const [selectedStatus, setSelectedStatus] = useState('Planned')
 
+    const { data: items } = useQuery({
+        queryFn: () => SAMPLE_FEEDBACK,
+        queryKey: ['feedback']
+    })
+
+    const plannedFeedback = items?.filter(item => item.status === 'Planned')
+    const inProgressFeedback = items?.filter(item => item.status === 'In-Progress')
+    const liveFeedback = items?.filter(item => item.status === 'Planned')
+
     return (
-        <section>
+        <section
+            className={styles.roadmap}
+        >
 
             <div
                 className={styles.status_bar}
@@ -18,7 +30,7 @@ export default function RoadmapGrid() {
                     onClick={() => setSelectedStatus('Planned')}
                 >
                     <h5>
-                        Planned (2)
+                        Planned ({plannedFeedback?.length || 0})
                     </h5>
                 </button>
 
@@ -27,7 +39,7 @@ export default function RoadmapGrid() {
                     onClick={() => setSelectedStatus('In-Progress')}
                 >
                     <h5>
-                        In-Progress (3)
+                        In-Progress ({inProgressFeedback?.length || 0})
                     </h5>
                 </button>
 
@@ -36,26 +48,104 @@ export default function RoadmapGrid() {
                     onClick={() => setSelectedStatus('Live')}
                 >
                     <h5>
-                        Live (1)
+                        Live ({liveFeedback?.length || 0})
                     </h5>
                 </button>
 
             </div>
 
-            <FeedbackCard
-                {...SAMPLE_FEEDBACK[0]}
-                statused
-            />
+            <div
+                className={styles.grid}
+                data-selected={selectedStatus === 'Planned'}
+            >
 
-            <FeedbackCard
-                {...SAMPLE_FEEDBACK[1]}
-                statused
-            />
+                <div
+                    className={styles.grid_head}
+                >
 
-            <FeedbackCard
-                {...SAMPLE_FEEDBACK[2]}
-                statused
-            />
+                    <h3>
+                        Planned ({plannedFeedback?.length || 0})
+                    </h3>
+
+                    <p>
+                        Ideas prioritized for research
+                    </p>
+
+                </div>
+
+                {
+                    plannedFeedback?.map(item => (
+                        <FeedbackCard
+                            key={item.id}
+                            {...item}
+                            statused
+                        />
+                    ))
+                }
+
+            </div>
+
+            <div
+                className={styles.grid}
+                data-selected={selectedStatus === 'In-Progress'}
+            >
+
+                <div
+                    className={styles.grid_head}
+                >
+
+                    <h3>
+                        In-Progress ({inProgressFeedback?.length || 0})
+                    </h3>
+
+                    <p>
+                        Currently being developed
+                    </p>
+
+                </div>
+
+                {
+                    inProgressFeedback?.map(item => (
+                        <FeedbackCard
+                            key={item.id}
+                            {...item}
+                            statused
+                        />
+                    ))
+                }
+
+            </div>
+
+            <div
+                className={styles.grid}
+                data-selected={selectedStatus === 'Live'}
+            >
+
+                <div
+                    className={styles.grid_head}
+                >
+
+                    <h3>
+                        Live ({liveFeedback?.length || 0})
+                    </h3>
+
+                    <p>
+                        Released features
+                    </p>
+
+                </div>
+
+                {
+                    liveFeedback?.map(item => (
+                        <FeedbackCard
+                            key={item.id}
+                            {...item}
+                            statused
+                        />
+                    ))
+                }
+
+            </div>
 
         </section>
     )
