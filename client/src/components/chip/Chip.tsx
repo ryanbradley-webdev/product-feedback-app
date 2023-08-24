5b8089565bd45849357b81043a6b30e9cb048cf2
Ryan Bradley <ryanbradley315@gmail.com>
2023-08-24 12:57:47 -0500

    Add hover effects to chip, upvote, and card components.

--- client/src/components/chip/Chip.tsx
+++ client/src/components/chip/Chip.tsx
@@ -39,6 +39,7 @@ export default function Chip({
     return (
         <label
             className={styles.chip}
+            data-clickable={setFilters !== undefined}
             {...props}
         >
 
